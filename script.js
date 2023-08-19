a1035d048d0ffec93229b6374446d71312cbd22d
candicoded <103226511+candicoded@users.noreply.github.com>
2023-08-19 11:53:04 -0400

    added active to rating buttons

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ const submitButton = document.querySelector('.submit');
 const ratings = document.querySelectorAll('.number');
 const actualRating = document.getElementById('rating');
 
+ratings.forEach((rating) => {
+  rating.addEventListener('click', () => {
+    actualRating.innerHTML = rating.innerHTML;
+    ratings.forEach((rating) => {
+      rating.classList.remove('active');
+    });
+      rating.classList.add('active');
+      submitButton.disabled = false;
+  }); 
+});
+
 submitButton.addEventListener('click', () => {
   mainContainer.classList.add('hidden');
   thanksContainer.classList.remove('hidden');
 });
-
-ratings.forEach((rating) => {
-  rating.addEventListener('click', () => {
-    actualRating.innerHTML = rating.innerHTML;
-}); 
-}); 
